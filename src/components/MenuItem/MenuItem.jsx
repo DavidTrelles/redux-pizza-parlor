2a68fd900bf5da4025b3948a86ec0da8186f3594
David Trelles <davidstrelles@gmail.com>
2023-02-07 17:13:16 -0600

    added quantity to pizza object

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,52 +1,51 @@
 import { useDispatch } from "react-redux";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-function MenuItem({pizza}){
+function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const addPizza = () => {
+    dispatch({
+      type: "ADD_PIZZA",
+      payload: { ...pizza, quantity: 1 },
+    });
+    dispatch({
+      type: "ADD_TO_TOTAL",
+      payload: Number(pizza.price),
+    });
+    console.log("price for", pizza.name, "is", pizza.price);
+    toggleCart();
+  };
 
-    const addPizza = () => {
-        dispatch({
-            type: 'ADD_PIZZA',
-            payload: pizza
-        });
-        dispatch({
-            type: 'ADD_TO_TOTAL',
-            payload: Number(pizza.price)
-        })
-        console.log('price for', pizza.name, 'is', pizza.price);
-        toggleCart();
-    }
+  const removePizza = () => {
+    console.log("REMOVE");
+    toggleCart();
+  };
 
-    const removePizza = () => {
-        console.log('REMOVE');
-        toggleCart();
-    }
+  const [inCart, setInCart] = useState(false);
 
-    const [inCart, setInCart] = useState(false);
+  const toggleCart = () => {
+    setInCart(!inCart);
+  };
 
-    const toggleCart = () => {
-        setInCart(!inCart);
-    }
-
-
-    return(
-        <div className="menuBox">
-            <h3>{pizza.name}</h3>
-            <div className="photoBox">
-                <img className="photo" key={pizza.id} src={pizza.image_path} ></img>
-            </div>
-            <div className="description">
-                <p>{pizza.description}</p>
-                <p>{pizza.price}</p>
-            </div>
-            {/* <button onClick={addPizza}>ADD TO CART</button> */}
-            {inCart ?
-           <button onClick={removePizza}>REMOVE FROM CART</button>:
-           <button onClick={addPizza}>ADD TO CART</button>}
-    
-        </div>
-    )
+  return (
+    <div className="menuBox">
+      <h3>{pizza.name}</h3>
+      <div className="photoBox">
+        <img className="photo" key={pizza.id} src={pizza.image_path}></img>
+      </div>
+      <div className="description">
+        <p>{pizza.description}</p>
+        <p>{pizza.price}</p>
+      </div>
+      {/* <button onClick={addPizza}>ADD TO CART</button> */}
+      {inCart ? (
+        <button onClick={removePizza}>REMOVE FROM CART</button>
+      ) : (
+        <button onClick={addPizza}>ADD TO CART</button>
+      )}
+    </div>
+  );
 }
 
 export default MenuItem;
